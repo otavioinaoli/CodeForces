46954ac36d2198082f794e50a67b732d089c24d9
Seu Nome <otaviogame25@gmail.com>
2025-08-24 04:08:29 -0300

    BeautifulMatrix.cpp

---
+++ A/BeautifulMatrix.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    vector<vector<int>> matrix(5, vector<int>(5));
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            cin >> matrix[i][j];
+        }
+    }
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if(matrix[i][j] == 1)
+                cout << abs(2 - i) + abs(2 - j);
+        }
+    }
+
+
+    return 0;
+
+}
